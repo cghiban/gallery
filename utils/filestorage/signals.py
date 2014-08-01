e1a6a38cf488f86548b921b9d8726effc44ab526
Tim Shaffer <timshaffer@me.com>
2014-08-01 14:29:12 -0400

    Refactor filestorage signal.

--- utils/filestorage/signals.py
+++ utils/filestorage/signals.py
@@ -11,12 +11,8 @@ def delete_files_on_delete(sender, instance, **kwargs):
             continue
         file_field = getattr(instance, field.name)
         storage = file_field.storage
-        try:
-            path = file_field.path
-        except ValueError:
-            # This means that for some reason the file is already deleted.
-            return False
-        storage.delete(path)
+        if file_field and storage and storage.exists(file_field.name):
+            storage.delete(file_field.name)
 
 
 def delete_files_on_change(sender, instance, **kwargs):
@@ -24,22 +20,18 @@ def delete_files_on_change(sender, instance, **kwargs):
     Deletes old file from filesystem when a file is changed on an instance.
     """
     if not instance.pk:
-        return False
+        return
 
     try:
         old_instance = instance.__class__.objects.get(pk=instance.pk)
-    except instance.__class__.DoesNotExist:
-        return False
+    except instance.DoesNotExist:
+        return
 
     for field in instance._meta.fields:
         if not isinstance(field, FileField):
             continue
-        old_file_field = getattr(old_instance, field.name)
-        new_file_field = getattr(instance, field.name)
-        try:
-            path = old_file_field.path
-        except ValueError:
-            # This means that for some reason the file is already deleted.
-            return False
-        if old_file_field.name != new_file_field.name:
-            old_file_field.storage.delete(path)
+        old_file = getattr(old_instance, field.name)
+        new_file = getattr(instance, field.name)
+        storage = old_file.storage
+        if old_file != new_file and storage and storage.exists(old_file.name):
+            storage.delete(old_file.name)
