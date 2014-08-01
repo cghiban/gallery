a2b94bb3c0a7b4cb0c17286ade5a2f6ab35523ff
Tim Shaffer <timshaffer@me.com>
2014-08-01 14:45:32 -0400

    Reverted change since it was not good.

--- utils/filestorage/signals.py
+++ utils/filestorage/signals.py
@@ -11,8 +11,12 @@ def delete_files_on_delete(sender, instance, **kwargs):
             continue
         file_field = getattr(instance, field.name)
         storage = file_field.storage
-        if file_field and storage and storage.exists(file_field.name):
-            storage.delete(file_field.name)
+        try:
+            path = file_field.path
+        except ValueError:
+            # This means that for some reason the file is already deleted.
+            return False
+        storage.delete(path)
 
 
 def delete_files_on_change(sender, instance, **kwargs):
@@ -20,18 +24,22 @@ def delete_files_on_change(sender, instance, **kwargs):
     Deletes old file from filesystem when a file is changed on an instance.
     """
     if not instance.pk:
-        return
+        return False
 
     try:
         old_instance = instance.__class__.objects.get(pk=instance.pk)
-    except instance.DoesNotExist:
-        return
+    except instance.__class__.DoesNotExist:
+        return False
 
     for field in instance._meta.fields:
         if not isinstance(field, FileField):
             continue
-        old_file = getattr(old_instance, field.name)
-        new_file = getattr(instance, field.name)
-        storage = old_file.storage
-        if old_file != new_file and storage and storage.exists(old_file.name):
-            storage.delete(old_file.name)
+        old_file_field = getattr(old_instance, field.name)
+        new_file_field = getattr(instance, field.name)
+        try:
+            path = old_file_field.path
+        except ValueError:
+            # This means that for some reason the file is already deleted.
+            return False
+        if old_file_field.name != new_file_field.name:
+            old_file_field.storage.delete(path)
